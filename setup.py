c7b87c872560352ff3506db1d1efe93d22481eee
Brooks Johnson <brooks.johnson@pm.me>
2025-04-13 14:18:47 -0700

    Undo setup changes
    I have a feeling this will cause a publish to fail because the platform
    tag isn't formatted properly. I will try to add this next.

--- setup.py
+++ setup.py
@@ -20,15 +20,6 @@ from setuptools.dist import Distribution
 #         return True
 
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
-
 if __name__ == "__main__":
     try:
         setup(
@@ -37,7 +28,6 @@ if __name__ == "__main__":
             package_data={
                 '':['libuplink.so', 'py.typed']
             },
-            cmdclass={'bdist_wheel': bdist_wheel},
             # distclass=BinaryDistribution
         )
     except:  # noqa
